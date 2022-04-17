2eb210d3fcb58f011376c3b26a89bc4b34c786ba
aleksandr.savkin <aleksandr.savkin@dicehub.com>
2022-04-17 14:19:49 +0200

    Add login page

--- pages/index.vue
+++ pages/index.vue
@@ -6,48 +6,18 @@
         nuxt-express
       </h1>
       <div>
-        {{ test }}
         <div class="links">
           <a
-            href="/users"
+            href="/login"
             class="button--green"
           >
-            Users List
+            Log in
           </a>
         </div>
-      </div>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
       </div>
     </div>
   </div>
 </template>
-
-<script>
-export default {
-  async asyncData ({ $http }) {
-    const test = await $http.$get('/api/test')
-    return {
-      test
-    }
-  }
-}
-</script>
 
 <style scoped>
 .container {
---
+++ pages/login/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="login-page">
+    <h1>Log in to visavi</h1>
+
+    <form @submit.prevent="submitLogin">
+      <label for="username">
+        <input id="username" v-model="form.username" autocomplete="username" type="text">
+      </label>
+      <label for="password">
+        <input id="password" v-model="form.password" autocomplete="password" type="password">
+      </label>
+      <button type="submit">
+        {{ $t('login.submit') }}
+      </button>
+      <span v-if="isLoggedIn">
+        Status: Logged in!
+      </span>
+      <span v-else>
+        Status: Not logged in!
+      </span>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      form: {
+        username: '',
+        password: ''
+      },
+      isLoggedIn: false
+    }
+  },
+
+  head () {
+    return {
+      title: this.$t('login.seo.title')
+    }
+  },
+
+  methods: {
+    submitLogin () {
+      const jsonBody = JSON.stringify({ username: this.form.username, password: this.form.password })
+
+      const url = '/api/login'
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          Accept: 'application/json'
+        },
+        body: jsonBody
+      })
+        .then((res) => {
+          return res.json()
+        })
+        .then((data) => {
+          if (data || data.status === '200') {
+            this.isLoggedIn = true
+          }
+        })
+    }
+  }
+}
+</script>
