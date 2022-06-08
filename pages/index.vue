5948ee1cf0432fe36ec200629a947fe73a20cff8
aleksandr.savkin <aleksandr.savkin@dicehub.com>
2022-06-08 07:05:21 +0300

    Add author block

--- pages/index.vue
+++ pages/index.vue
@@ -91,10 +91,66 @@
 
     <div class="author">
       <b-container>
-        <div class="author__header text-center">
-          <h2>
+        <div class="author__content desktop">
+          <div class="author__text">
+            <h2 class="author__title">
+              {{ $t('index.author_title') }}
+            </h2>
+
+            <div class="author__description">
+              <p>
+                {{ $t('index.author_description_1') }}
+              </p>
+              <p>
+                {{ $t('index.author_description_2') }}
+              </p>
+              <p>
+                {{ $t('index.author_description_3') }}
+              </p>
+              <div class="author__button">
+                <nuxt-link
+                  to="/#main-single-lessons"
+                  class="button button--beige button button--large"
+                >
+                  {{ $t('index.author_want_study') }}
+                </nuxt-link>
+              </div>
+            </div>
+          </div>
+          <div class="author__image">
+            <img src="@/assets/images/author-image.png">
+            <div class="author__signature">
+              <div>Viktoria</div>
+              <div>Kosto</div>
+            </div>
+          </div>
+        </div>
+        <div class="author__content mobile">
+          <h2 class="author__title">
             {{ $t('index.author_title') }}
           </h2>
+          <div class="author__flexbox">
+            <div class="author__description">
+              <p>
+                {{ $t('index.author_description_mobile') }}
+              </p>
+              <div class="author__signature">
+                <div>Viktoria</div>
+                <div>Kosto</div>
+              </div>
+            </div>
+            <div class="author__image">
+              <img src="@/assets/images/author-image.png">
+            </div>
+          </div>
+          <div class="author__button">
+            <nuxt-link
+              to="/#main-single-lessons"
+              class="button button--beige button button--large"
+            >
+              {{ $t('index.author_want_study') }}
+            </nuxt-link>
+          </div>
         </div>
       </b-container>
     </div>
@@ -327,9 +383,15 @@ export default {
     text-transform: uppercase;
     font-family: 'Cormorant SC', serif;
     font-weight: 700;
+    font-size: 2.5rem;
 
     @media screen and (max-width: 991px) {
-      font-size: 1.7rem;
+      text-align: center;
+      font-size: 2.2rem;
+    }
+
+    @media screen and (max-width: 480px) {
+      font-size: 1.6rem;
     }
   }
 
@@ -380,7 +442,109 @@ export default {
 }
 
 .author {
-  padding: 4rem 0;
+  padding: 2rem 0;
+
+  &__title {
+    text-transform: uppercase;
+    font-family: 'Cormorant SC', serif;
+    font-weight: 700;
+    font-size: 2.5rem;
+
+    @media screen and (max-width: 991px) {
+      font-size: 2.2rem;
+      text-align: center;
+    }
+
+    @media screen and (max-width: 480px) {
+      font-size: 1.6rem;
+    }
+  }
+
+  &__content.desktop {
+    display: flex;
+
+    @media screen and (max-width: 991px) {
+      display: none;
+    }
+  }
+
+   &__content.mobile {
+    display: none;
+
+    @media screen and (max-width: 991px) {
+      display: block;
+    }
+  }
+
+  .mobile &__flexbox {
+    display: flex;
+    margin-top: 2rem;
+  }
+
+  &__text {
+    padding-top: 4rem;
+  }
+
+  &__description {
+    .desktop & {
+      margin-top: 4rem;
+    }
+
+    .mobile & {
+      min-width: 180px;
+    }
+  }
+
+  &__image {
+    position: relative;
+    margin-left: 3rem;
+
+    .mobile & {
+      margin-left: 5px;
+      min-width: 170px;
+      margin-top: 1rem;
+
+      @media screen and (min-width: 768px) {
+        min-width: 250px;
+        margin-top: 0;
+      }
+
+      img {
+        width: 100%;
+        height: auto;
+      }
+    }
+
+  }
+
+  &__signature {
+    font-size: 3.5rem;
+    transform: rotate(-15deg);
+    font-weight: 500;
+    font-family: 'Haigrast', serif;
+    line-height: 100%;
+
+    .desktop & {
+      position: absolute;
+      left: -10rem;
+      bottom: 0;
+    }
+
+    .mobile & {
+      font-size: 3rem;
+    }
+  }
+
+  &__button {
+    display: inline-flex;
+    margin-top: 1.5rem;
+
+    .mobile & {
+      margin-top: 3rem;
+      display: flex;
+      justify-content: center;
+    }
+  }
 }
 
 .lessons {
