b63d2c5e6d1767e180ca20f6803efe45b2654067
aleksandr.savkin <aleksandr.savkin@dicehub.com>
2022-08-13 11:48:13 +0300

    Add after message webhook

--- api/routes/message.js
+++ api/routes/message.js
@@ -31,4 +31,23 @@ router.post('/message/notify', async function (req, res) {
   }
 })
 
+router.post('/message/after-message', async function (req, res) {
+  try {
+    const { data } = req.body
+    console.log('New message is caught (to): ', data.receiver)
+    console.log('New message is caught (body): ', data.data)
+    res.sendStatus(200)
+
+    const user = await UsersModel.findOne({ _id: data.receiver })
+    if (user && user.email && user.email === adminEmail) {
+      sendEmail(`У вас новое сообщение на сайте vikosto: ${data.data?.text}`, {
+        toEmail: user.email,
+        subject: 'У вас новое сообщение на сайте vikosto'
+      })
+    }
+  } catch (e) {
+    console.error(e)
+  }
+})
+
 module.exports = router
