ab52a468323909b1aa1a47868519c20c3f5863bb
aleksandr.savkin <aleksandr.savkin@dicehub.com>
2022-08-13 13:49:55 +0300

    Fix admin messages notification

--- api/routes/message.js
+++ api/routes/message.js
@@ -34,16 +34,22 @@ router.post('/message/notify', async function (req, res) {
 router.post('/message/after-message', async function (req, res) {
   try {
     const { data } = req.body
-    console.log('New message is caught (to): ', data.receiver)
-    console.log('New message is caught (body): ', data.data)
+    console.log('New message is caught: ', data)
     res.sendStatus(200)
 
-    const user = await UsersModel.findOne({ _id: data.receiver })
-    if (user && user.email && user.email === adminEmail) {
+    if (data.receiver === '1') {
       sendEmail(`У вас новое сообщение на сайте vikosto: ${data.data?.text}`, {
-        toEmail: user.email,
+        toEmail: adminEmail,
         subject: 'У вас новое сообщение на сайте vikosto'
       })
+    } else {
+      const user = await UsersModel.findOne({ _id: data.receiver })
+      if (user && user.email && user.email === adminEmail) {
+        sendEmail(`У вас новое сообщение на сайте vikosto: ${data.data?.text}`, {
+          toEmail: user.email,
+          subject: 'У вас новое сообщение на сайте vikosto'
+        })
+      }
     }
   } catch (e) {
     console.error(e)
